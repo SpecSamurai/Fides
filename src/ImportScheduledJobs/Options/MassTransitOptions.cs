64c8c7b2279964ff1eb106ec0cf7e6466c7e7376
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-12 15:32:57 +0200

    Add retry policy configuration to MassTransitOptions

--- src/ImportScheduledJobs/Options/MassTransitOptions.cs
+++ src/ImportScheduledJobs/Options/MassTransitOptions.cs
@@ -2,8 +2,17 @@ namespace ImportScheduledJobs.Options;
 
 public class MassTransitOptions
 {
+    public const int DefaultRetryLimit = 6;
+    public const int DefaultMinIntervalInSeconds = 0;
+    public const int DefaultMaxIntervalInSeconds = 60;
+    public const int DefaultIntervalDeltaInSeconds = 5;
+
     public string? Host { get; set; }
     public string? VirtualHost { get; set; }
     public string? UserName { get; set; }
     public string? Password { get; set; }
+    public int? RetryLimit { get; set; }
+    public int? MinIntervalInSeconds { get; set; }
+    public int? MaxIntervalInSeconds { get; set; }
+    public int? IntervalDeltaInSeconds { get; set; }
 }
