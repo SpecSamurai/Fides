f361ea35914f5d4c9250fe5ad0afc87e85e64c28
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-16 21:22:57 +0200

    Add QueueName to SyncOptions

--- src/ImportScheduledJobs/Options/SyncOptions.cs
+++ src/ImportScheduledJobs/Options/SyncOptions.cs
@@ -3,4 +3,8 @@ namespace ImportScheduledJobs.Options;
 public class SyncOptions
 {
     public int ImportPageSize { get; set; }
+    public string? QueueName { get; set; }
+
+    public Uri GetQueueUri() =>
+        new Uri($"queue:{QueueName ?? throw new ArgumentNullException($"{nameof(QueueName)} is null.")}");
 }
