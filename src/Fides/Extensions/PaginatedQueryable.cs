ba8327c849a2c7c2d964bdf1dff8a56a52222ce8
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-21 19:32:16 +0200

    Extend PaginatedQueryable to handle IQueryable that implements both IEnumerable and IAsyncEnumerable

--- src/Fides/Extensions/PaginatedQueryable.cs
+++ src/Fides/Extensions/PaginatedQueryable.cs
@@ -12,33 +12,39 @@ public class PaginatedQueryable<TType>
 
     public PaginatedQueryable(IQueryable<TType> source, int count, int pageSize)
     {
-        if (source is IAsyncEnumerable<TType>)
-        {
-            _source = source;
-            _pageSize = pageSize;
-            _totalPages = (int)Math.Ceiling(count / (double)pageSize);
-        }
-        else throw new ArgumentException(
-            $"IQueryable<{typeof(TType).Name}> {nameof(source)} doesn't implement IAsyncEnumerable<{typeof(TType).Name}>.");
+        _source = source;
+        _pageSize = pageSize;
+        _totalPages = (int)Math.Ceiling(count / (double)pageSize);
     }
 
     public bool HasNextPage =>
         _pageIndex < _totalPages;
 
-    public virtual async Task<IEnumerable<TResult>> NextPageAsync<TResult>(
+    public async Task<IEnumerable<TResult>> NextPageAsync<TResult>(
         Expression<Func<TType, TResult>> func,
-        CancellationToken cancellationToken = default) =>
-            HasNextPage
+        CancellationToken cancellationToken = default)
+    {
+        if (HasNextPage)
+        {
+            return _source is IAsyncEnumerable<TType>
                 ? await _source
                     .Skip(_pageIndex++ * _pageSize)
                     .Take(_pageSize)
                     .Select(func)
                     .ToListAsync(cancellationToken)
                     .ConfigureAwait(false)
-                : Enumerable.Empty<TResult>();
-
-    public static PaginatedQueryable<TType> Empty()
-    {
-        return new PaginatedQueryable<TType>(Enumerable.Empty<TType>().AsQueryable(), count: 0, pageSize: 0);
+                : NextPage(func);
+        }
+        else return Enumerable.Empty<TResult>();
     }
+
+    private IEnumerable<TResult> NextPage<TResult>(Expression<Func<TType, TResult>> func) =>
+        _source
+            .Skip(_pageIndex++ * _pageSize)
+            .Take(_pageSize)
+            .Select(func)
+            .ToList();
+
+    public static PaginatedQueryable<TType> Empty() =>
+        new PaginatedQueryable<TType>(Enumerable.Empty<TType>().AsQueryable(), count: 0, pageSize: 0);
 }
