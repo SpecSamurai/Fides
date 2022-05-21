5bb0f758c87812df0833b933407c6d0b8cfad3b2
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-21 12:40:22 +0200

    Add error logs and exception handling to OrderItemRepository

--- src/ImportScheduledJobs/Repositories/OrderItemRepository.cs
+++ src/ImportScheduledJobs/Repositories/OrderItemRepository.cs
@@ -7,15 +7,22 @@ namespace ImportScheduledJobs.Repositories;
 
 public class OrderItemRepository : IOrderItemRepository
 {
+    private readonly ILogger<OrderItemRepository> _logger;
     private readonly StoresDbContext _storesDbContext;
 
-    public OrderItemRepository(StoresDbContext storesDbContext) =>
+    public OrderItemRepository(ILogger<OrderItemRepository> logger, StoresDbContext storesDbContext)
+    {
+        _logger = logger;
         _storesDbContext = storesDbContext;
+    }
 
     public async Task<PaginatedQueryable<OrderItem>> GetOrdersSortedByBrandAndPriceAync(
         int pageSize,
-        IQueryObject<OrderItem, bool> where) =>
-            await _storesDbContext
+        IQueryObject<OrderItem, bool> where)
+    {
+        try
+        {
+            return await _storesDbContext
                 .OrderItems
                 .AsNoTracking()
                 .Include(orderItem => orderItem.Order)
@@ -28,4 +35,11 @@ public class OrderItemRepository : IOrderItemRepository
                 .OrderBy(orderItem => orderItem.Product.BrandId)
                 .ThenByDescending(orderItem => orderItem.ListPrice)
                 .ToPaginatedAsync(pageSize);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Unexpected error.");
+            return PaginatedQueryable<OrderItem>.Empty();
+        }
+    }
 }
