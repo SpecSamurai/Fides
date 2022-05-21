2d82bc44f903bb4b200013d29173bebadc9524cd
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-21 12:41:49 +0200

    Improve DeleteMessageConsumer - Add exceptions handling - Use IOrderedItemIndexingRepository

--- src/ImportScheduledJobs/Consumers/DeleteMessageConsumer.cs
+++ src/ImportScheduledJobs/Consumers/DeleteMessageConsumer.cs
@@ -1,24 +1,39 @@
+using ImportScheduledJobs.Repositories;
 using MassTransit;
-using Nest;
 
 namespace ImportScheduledJobs.Consumers;
 
 public class DeleteMessageConsumer : IConsumer<Batch<DeleteMessage>>
 {
     private readonly ILogger<DeleteMessageConsumer> _logger;
-    private readonly IElasticClient _elasticClient;
+    private readonly IOrderedItemIndexingRepository _orderedItemIndexingRepository;
 
     public DeleteMessageConsumer(
         ILogger<DeleteMessageConsumer> logger,
-        IElasticClient elasticClient)
+        IOrderedItemIndexingRepository orderedItemIndexingRepository)
     {
-        this._logger = logger;
-        this._elasticClient = elasticClient;
+        _logger = logger;
+        _orderedItemIndexingRepository = orderedItemIndexingRepository;
     }
 
     public async Task Consume(ConsumeContext<Batch<DeleteMessage>> context)
     {
-        var items = context.Message.Select(message => message.Message.OrderedItem);
-        var indexManyResponse = await _elasticClient.DeleteManyAsync(items);
+        try
+        {
+            var orderedItems = context.Message
+                    .Select(message => message.Message.OrderedItem)
+                    .Where(item => item is not null);
+
+            if (orderedItems.Any())
+                await _orderedItemIndexingRepository.DeleteDocuments(orderedItems);
+        }
+        catch (TransportException exception)
+        {
+            _logger.LogError(exception, "Error occurred while deleting documents.");
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Unexpected error.");
+        }
     }
 }
