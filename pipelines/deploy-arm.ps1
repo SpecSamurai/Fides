0bab4ef263bd493ff12f6ed30446e1040349494b
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-26 14:43:07 +0200

    Fix references to Template files in the deploy-arm script

--- pipelines/deploy-arm.ps1
+++ pipelines/deploy-arm.ps1
@@ -5,19 +5,19 @@ $subscription = Get-AzSubscription
 New-AzSubscriptionDeployment `
     -Name 'deployResourceGroup' `
     -Location westeurope `
-    -TemplateFile "..\arm\resource-group\template.json" `
-    -TemplateParameterFile "..\arm\resource-group\parameters.json" `
+    -TemplateFile "arm\resource-group\template.json" `
+    -TemplateParameterFile "arm\resource-group\parameters.json" `
     -rgName $resourceGroupName `
 
 New-AzResourceGroupDeployment `
     -Name deployRegistry `
     -ResourceGroupName $resourceGroupName `
-    -TemplateFile "..\arm\registry\template.json" `
-    -TemplateParameterFile "..\arm\registry\parameters.json"
+    -TemplateFile "arm\registry\template.json" `
+    -TemplateParameterFile "arm\registry\parameters.json"
 
 New-AzResourceGroupDeployment `
     -Name deployFunction `
     -ResourceGroupName $resourceGroupName `
-    -TemplateFile "..\arm\sync-function\template.json" `
-    -TemplateParameterFile "..\arm\sync-function\parameters.json" `
+    -TemplateFile "arm\sync-function\template.json" `
+    -TemplateParameterFile "arm\sync-function\parameters.json" `
     -subscriptionId $subscription.Id
