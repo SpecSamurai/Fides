6d05b889d9cb4c30224531653185401942cd4861
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-30 17:26:11 +0200

    Fix references to Dockerfiles

--- infrastructure/pipelines/deploy-containers.sh
+++ infrastructure/pipelines/deploy-containers.sh
@@ -1,14 +1,14 @@
 az acr build --image fides/elasticsearch:v1 \
     --registry containerregistryfides \
-    --file Dockerfile ./deployment/containers/elk/elasticsearch
+    --file Dockerfile ./infrastructure/containers/elk/elasticsearch
 
 az acr build --image fides/kibana:v1 \
     --registry containerregistryfides \
-    --file Dockerfile ./deployment/containers/elk/kibana
+    --file Dockerfile ./infrastructure/containers/elk/kibana
 
 az acr build --image fides/logstash:v1 \
     --registry containerregistryfides \
-    --file Dockerfile ./deployment/containers/elk/logstash
+    --file Dockerfile ./infrastructure/containers/elk/logstash
 
 # az container create --resource-group Fides --file ./deployment/containers/sql-template.yaml
 # az container create --resource-group Fides --file ./deployment/containers/rabbitmq-template.yaml
