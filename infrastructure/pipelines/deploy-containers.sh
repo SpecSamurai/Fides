4c45792e19e07db6f0c02f7a0a6d7be6f406ad26
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-30 17:36:05 +0200

    Add SOURCE_LOCATION

--- infrastructure/pipelines/deploy-containers.sh
+++ infrastructure/pipelines/deploy-containers.sh
@@ -1,14 +1,14 @@
 az acr build --image fides/elasticsearch:v1 \
     --registry containerregistryfides \
-    --file ./infrastructure/containers/elk/elasticsearch/Dockerfile
+    --file ./infrastructure/containers/elk/elasticsearch/Dockerfile .
 
 az acr build --image fides/kibana:v1 \
     --registry containerregistryfides \
-    --file ./infrastructure/containers/elk/kibana/Dockerfile
+    --file ./infrastructure/containers/elk/kibana/Dockerfile .
 
 az acr build --image fides/logstash:v1 \
     --registry containerregistryfides \
-    --file ./infrastructure/containers/elk/logstash/Dockerfile 
+    --file ./infrastructure/containers/elk/logstash/Dockerfile .
 
 # az container create --resource-group Fides --file ./deployment/containers/sql-template.yaml
 # az container create --resource-group Fides --file ./deployment/containers/rabbitmq-template.yaml
