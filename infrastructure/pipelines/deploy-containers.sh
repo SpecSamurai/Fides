97715c811611a4b7186c0839204ab87ea81af21a
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-31 17:52:37 +0200

    Adjust deploy containers script to use ARM templates

--- infrastructure/pipelines/deploy-containers.sh
+++ infrastructure/pipelines/deploy-containers.sh
@@ -1,15 +1,29 @@
-az acr build --image fides/elasticsearch:v1 \
+az acr build --image elasticsearch:v1 \
     --registry containerregistryfides \
     --file ./infrastructure/containers/elk/elasticsearch/Dockerfile ./infrastructure/containers/elk/elasticsearch/
 
-az acr build --image fides/kibana:v1 \
+az acr build --image kibana:v1 \
     --registry containerregistryfides \
     --file ./infrastructure/containers/elk/kibana/Dockerfile ./infrastructure/containers/elk/kibana/
 
-az acr build --image fides/logstash:v1 \
+az acr build --image logstash:v1 \
     --registry containerregistryfides \
     --file ./infrastructure/containers/elk/logstash/Dockerfile ./infrastructure/containers/elk/logstash/
 
-# az container create --resource-group Fides --file ./deployment/containers/sql-template.yaml
-# az container create --resource-group Fides --file ./deployment/containers/rabbitmq-template.yaml
-# az container create --resource-group Fides --file ./deployment/containers/elk-template.yaml
+az deployment group create \
+  --name deploySQL \
+  --resource-group Fides \
+  --template-file "infrastructure\containers\arm\sql-template\template.json" \
+  --parameters "infrastructure\containers\arm\sql-template\parameters.json"
+
+az deployment group create \
+  --name deployRabbitMQ \
+  --resource-group Fides \
+  --template-file "infrastructure\containers\arm\rabbitmq-template\template.json" \
+  --parameters "infrastructure\containers\arm\rabbitmq-template\parameters.json"
+
+az deployment group create \
+  --name deployELK \
+  --resource-group Fides \
+  --template-file "infrastructure\containers\arm\elk-template\template.json" \
+  --parameters "infrastructure\containers\arm\elk-template\parameters.json"
