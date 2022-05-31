c322577137e8da85160aa9de559de2c924b43252
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-31 13:02:38 +0200

    Move pipelines configuration to infrestructure folder

---
+++ infrastructure/pipelines/deploy-arm.ps1
@@ -0,0 +1,23 @@
+$resourceGroupName = 'Fides'
+
+$subscription = Get-AzSubscription
+
+New-AzSubscriptionDeployment `
+    -Name 'deployResourceGroup' `
+    -Location westeurope `
+    -TemplateFile "infrastructure\arm\resource-group\template.json" `
+    -TemplateParameterFile "infrastructure\arm\resource-group\parameters.json" `
+    -rgName $resourceGroupName `
+
+New-AzResourceGroupDeployment `
+    -Name deployRegistry `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile "infrastructure\arm\registry\template.json" `
+    -TemplateParameterFile "infrastructure\arm\registry\parameters.json"
+
+New-AzResourceGroupDeployment `
+    -Name deployFunction `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile "infrastructure\arm\sync-function\template.json" `
+    -TemplateParameterFile "infrastructure\arm\sync-function\parameters.json" `
+    -subscriptionId $subscription.Id
