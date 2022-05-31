c64b0493c8b968b3fc4e7074752271f390db17c0
Spec <dawid.spec.jachowicz@gmail.com>
2022-05-31 12:23:22 +0200

    Delete ARM templates and containers YAML files

--- deployment/deploy-infrastructure.sh
+++
@@ -1,3 +0,0 @@
-az container create --resource-group Fides --file ./deployment/containers/sql-template.yaml
-az container create --resource-group Fides --file ./deployment/containers/rabbitmq-template.yaml
-az container create --resource-group Fides --file ./deployment/containers/elk-template.yaml --environment-variables discovery.type=single-node
