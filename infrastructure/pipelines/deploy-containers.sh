b0d0a1c7a6c0941f4a254425b70d098c4d44eb06
Spec <dawid.spec.jachowicz@gmail.com>
2022-06-11 18:54:04 +0200

    Add input arguments to deploy-containers script

--- infrastructure/pipelines/deploy-containers.sh
+++ infrastructure/pipelines/deploy-containers.sh
@@ -1,29 +1,48 @@
+#!/bin/bash
+while [ $# -gt 0 ] ; do
+  case $1 in
+    -RegistryName) RegistryName="$2" ;;
+    -ResourceGroupName) ResourceGroupName="$2" ;;
+    -KeyVaultName) KeyVaultName="$2" ;;
+    -Location) Location="$2" ;;
+    -ElkContainerName) ElkContainerName="$2" ;;
+    -SqlContainerName) SqlContainerName="$2" ;;
+    -RabbitMqContainerName) RabbitMqContainerName="$2" ;;
+    -ImageRegistryLoginServer) ImageRegistryLoginServer="$2" ;;
+
+  esac
+  shift
+done
+
 az acr build --image elasticsearch:v1 \
-  --registry containerregistryfides \
+  --registry $RegistryName \
   --file ./infrastructure/containers/elk/elasticsearch/Dockerfile ./infrastructure/containers/elk/elasticsearch/
 
 az acr build --image kibana:v1 \
-  --registry containerregistryfides \
+  --registry $RegistryName \
   --file ./infrastructure/containers/elk/kibana/Dockerfile ./infrastructure/containers/elk/kibana/
 
 az acr build --image logstash:v1 \
-  --registry containerregistryfides \
+  --registry $RegistryName \
   --file ./infrastructure/containers/elk/logstash/Dockerfile ./infrastructure/containers/elk/logstash/
 
 az deployment group create \
   --name deploySQL \
-  --resource-group Fides \
+  --resource-group $ResourceGroupName \
   --template-file "infrastructure\containers\arm\sql-template\template.json" \
-  --parameters "infrastructure\containers\arm\sql-template\parameters.json"
+  --parameters "infrastructure\containers\arm\sql-template\parameters.json" \
+  --parameters vaultName=$KeyVaultName vaultResourceGroupName=$ResourceGroupName location=$Location containerName=$SqlContainerName
 
 az deployment group create \
   --name deployRabbitMQ \
-  --resource-group Fides \
+  --resource-group $ResourceGroupName \
   --template-file "infrastructure\containers\arm\rabbitmq-template\template.json" \
-  --parameters "infrastructure\containers\arm\rabbitmq-template\parameters.json"
+  --parameters "infrastructure\containers\arm\rabbitmq-template\parameters.json" \
+  --parameters vaultName=$KeyVaultName vaultResourceGroupName=$ResourceGroupName location=$Location containerName=$RabbitMqContainerName
 
 az deployment group create \
   --name deployELK \
-  --resource-group Fides \
+  --resource-group $ResourceGroupName \
   --template-file "infrastructure\containers\arm\elk-template\template.json" \
-  --parameters "infrastructure\containers\arm\elk-template\parameters.json"
+  --parameters "infrastructure\containers\arm\elk-template\parameters.json" \
+  --parameters vaultName=$KeyVaultName vaultResourceGroupName=$ResourceGroupName location=$Location containerName=$ElkContainerName imageRegistryLoginServer=$ImageRegistryLoginServer
